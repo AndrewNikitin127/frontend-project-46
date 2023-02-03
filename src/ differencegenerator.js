2a3567f94bb78179a3cd56b28ed10c75ba09aae3
android127 <donandrain153@gmail.com>
2023-02-03 13:49:28 +0300

    fix differencegenerator

--- src/ differencegenerator.js
+++ src/ differencegenerator.js
@@ -11,33 +11,25 @@ const readFile = (filePath) => {
 const objectCompare = (object1, object2) => {
   const allSortsKeys = _.sortBy(Object.keys({ ...object1, ...object2 }));
 
-  const comparisonResultArray = allSortsKeys.reduce((result, key) => {
+  return allSortsKeys.reduce((result, key) => {
     if (_.has(object1, key) && !_.has(object2, key)) {
       result.push(`  - ${key}: ${object1[key]}`);
-    }
-    if (!_.has(object1, key) && _.has(object2, key)) {
+    } else if (!_.has(object1, key) && _.has(object2, key)) {
       result.push(`  + ${key}: ${object2[key]}`);
-    }
-    if (_.has(object1, key) && _.has(object2, key)) {
-      if (object1[key] === object2[key]) {
-        result.push(`    ${key}: ${object1[key]}`);
-      }
-      if (object1[key] !== object2[key]) {
-        result.push(`  - ${key}: ${object1[key]}`);
-        result.push(`  + ${key}: ${object2[key]}`);
-      }
+    } else if (object1[key] !== object2[key]) {
+      result.push(`  - ${key}: ${object1[key]}`);
+      result.push(`  + ${key}: ${object2[key]}`);
+    } else {
+      result.push(`    ${key}: ${object1[key]}`);
     }
     return result;
   }, []);
-
-  const resultAsString = ['{', ...comparisonResultArray, '}'].join('\n');
-  return resultAsString;
 };
 
 const genDiff = (path1, path2) => {
   const data1 = JSON.parse(readFile(path1));
   const data2 = JSON.parse(readFile(path2));
-  return objectCompare(data1, data2);
+  return `{\n${objectCompare(data1, data2)}\n}`;
 };
 
 export default genDiff;
