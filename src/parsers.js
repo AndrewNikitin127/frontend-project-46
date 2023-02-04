68cca60372f724b1c9631348cf6e01194689beb8
android127 <donandrain153@gmail.com>
2023-02-04 17:03:35 +0300

    little fix

--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,8 @@ import yaml from 'js-yaml';
 
 const parse = (data, format) => {
   if (format === '.json') return JSON.parse(data);
-  if (format === '.yaml' || format === '.yml') {
-    return yaml.safeload(data);
-  } return 'unknown format';
+  if (format === '.yaml' || format === '.yml') return yaml.safeload(data);
+  return 'unknown format';
 };
 
 export default parse;
