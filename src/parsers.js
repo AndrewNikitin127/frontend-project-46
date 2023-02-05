e7cf84fa1370f913dc26fc851f4133ace0a42eac
Dushik <donandrain153@gmail.com>
2023-02-06 00:30:01 +0300

    rm logs

--- src/parsers.js
+++ src/parsers.js
@@ -6,17 +6,4 @@ const parse = (data, format) => {
   return 'unknown format';
 };
 
-const obj1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false
-}
-
-const obj2 = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io'
-}
-
 export default parse;
