be7cef1c031baea23d5356cbb52c81de31535822
Dushik <donandrain153@gmail.com>
2023-02-05 23:30:50 +0300

    del consol.logs

--- src/parsers.js
+++ src/parsers.js
@@ -6,17 +6,4 @@ const parse = (data, format) => {
   return 'unknown format';
 };
 
-/*
-const a = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-  arr: [1,2,'ff',true],
-}
-
-console.log(yaml.dump(a)); */
-let a = [1,2,3,'2',true];
-console.log(`${a}`);
-
 export default parse;
