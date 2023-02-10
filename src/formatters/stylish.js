50a21f0f09b378bbdbb8a63d304f65e33bbd7272
android127 <donandrain153@gmail.com>
2023-02-10 19:37:41 +0300

    add finaly reff stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -10,25 +10,25 @@ const statDisplay = {
 const isNotNode = (val) => !_.has(val, 'type');
 const isLeafNode = (val) => _.has(val, 'value');
 
-const getNodeLine = (node, indent, spaceCount, iterFunc) => {
+const getNodeLine = (iterFunc, node, indent, spaceCount, indentProg = 2) => {
   if (node.type === 'changed') {
-    const oldline = `${indent}- ${node.name}: ${iterFunc(node.oldValue, spaceCount + 2)}`;
-    const newLine = `${indent}+ ${node.name}: ${iterFunc(node.newValue, spaceCount + 2)}`;
+    const oldline = `${indent}- ${node.name}: ${iterFunc(node.oldValue, spaceCount + indentProg)}`;
+    const newLine = `${indent}+ ${node.name}: ${iterFunc(node.newValue, spaceCount + indentProg)}`;
     return `${oldline}\n${newLine}`;
   }
-  return `${indent}${statDisplay[node.type]}${node.name}: ${iterFunc(node, spaceCount + 2)}`;
+  return `${indent}${statDisplay[node.type]}${node.name}: ${iterFunc(node, spaceCount + indentProg)}`;
 };
 
-const objectStringify = (data, replacer = ' ', spaceCount = 1) => {
-  const iter = (currentvalue, count = 1) => {
+const objectStringify = (data, replacer = ' ', spaceCount = 1, indentProg = 2) => {
+  const iter = (currentvalue, innerSaceCount = 1) => {
     if (!_.isObject(currentvalue)) return `${currentvalue}`;
 
-    const currentIndent = replacer.repeat(count);
-    const bracketSpaceCounter = count - 1;
+    const currentIndent = replacer.repeat(innerSaceCount);
+    const bracketSpaceCounter = innerSaceCount - 1;
     const brackeIndent = replacer.repeat(bracketSpaceCounter);
 
     const lines = Object.entries(currentvalue).map(
-      ([key, val]) => `${currentIndent}${statDisplay.unchanged}${key}: ${iter(val, count + 2)}`,
+      ([key, val]) => `${currentIndent}  ${key}: ${iter(val, innerSaceCount + indentProg)}`,
     );
 
     const result = ['{', ...lines, `${brackeIndent}}`].join('\n');
@@ -37,24 +37,24 @@ const objectStringify = (data, replacer = ' ', spaceCount = 1) => {
   return iter(data, spaceCount);
 };
 
-const buildStylishForm = (diffTree) => {
-  const replacer = '  ';
-
-  const iter = (currentvalue, spaceCount = 1) => {
+const buildStylishForm = (diffTree, replacer = '  ', indentationProgress = 2) => {
+  const iter = (currentValue, spaceCount = 1) => {
     const currentIndent = replacer.repeat(spaceCount);
     const bracketSpaceCounter = spaceCount - 1;
     const brackeIndent = replacer.repeat(bracketSpaceCounter);
 
-    if (isNotNode(currentvalue)) {
-      return theseisObjects(currentvalue)
-        ? objectStringify(currentvalue, replacer, spaceCount) : `${currentvalue}`;
+    if (isNotNode(currentValue)) {
+      return theseisObjects(currentValue)
+        ? objectStringify(currentValue, replacer, spaceCount, indentationProgress)
+        : `${currentValue}`;
     }
-    if (isLeafNode(currentvalue)) {
-      return theseisObjects(currentvalue.value)
-        ? objectStringify(currentvalue.value, replacer, spaceCount) : `${currentvalue.value}`;
+    if (isLeafNode(currentValue)) {
+      return theseisObjects(currentValue.value)
+        ? objectStringify(currentValue.value, replacer, spaceCount, indentationProgress)
+        : `${currentValue.value}`;
     }
-    const children = currentvalue.children.map(
-      (child) => getNodeLine(child, currentIndent, spaceCount, iter),
+    const children = currentValue.children.map(
+      (child) => getNodeLine(iter, child, currentIndent, spaceCount, indentationProgress),
     );
     return ['{', ...children, `${brackeIndent}}`].join('\n');
   };
