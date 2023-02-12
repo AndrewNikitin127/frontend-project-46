f4128fc9916ecc336da828cf9c343fb5be25f77e
android127 <donandrain153@gmail.com>
2023-02-12 17:01:50 +0300

    add fix reassignment buildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,32 +1,33 @@
 import _ from 'lodash';
 import { isObject } from './tools.js';
 
-const getStatus = (obj1, obj2, key) => {
+/* const getStatus = (obj1, obj2, key) => {
   if (!_.has(obj1, key)) return 'added';
   if (!_.has(obj2, key)) return 'removed';
   if (_.isEqual(obj1[key], obj2[key])) return 'unchanged';
   return 'changed';
 };
-
+*/
 const buildDiffTree = (object1, object2) => {
   const buildDiffObjects = (obj1, obj2) => {
     const allSortsKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 
     return allSortsKeys.flatMap((key) => {
-      const acc = { name: key, type: getStatus(obj1, obj2, key) };
-
       if (isObject(obj1[key]) && isObject(obj2[key])) {
-        acc.type = 'unchanged';
-        acc.children = buildDiffObjects(obj1[key], obj2[key]);
-      } else if (acc.type === 'added') {
-        acc.value = obj2[key];
-      } else if (acc.type === 'removed' || acc.type === 'unchanged') {
-        acc.value = obj1[key];
-      } else {
-        acc.oldValue = obj1[key];
-        acc.newValue = obj2[key];
+        return { name: key, type: 'unchanged', children: buildDiffObjects(obj1[key], obj2[key]) };
       }
-      return acc;
+      if (!_.has(obj1, key)) {
+        return { name: key, type: 'added', value: obj2[key] };
+      }
+      if (!_.has(obj2, key)) {
+        return { name: key, type: 'removed', value: obj1[key] };
+      }
+      if (_.isEqual(obj1[key], obj2[key])) {
+        return { name: key, type: 'unchanged', value: obj1[key] };
+      }
+      return {
+        name: key, type: 'changed', oldValue: obj1[key], newValue: obj2[key],
+      };
     });
   };
 
