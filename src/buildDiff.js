025df63bab73e1207215cc01725a2032b6c23d24
android127 <donandrain153@gmail.com>
2023-02-12 18:42:03 +0300

    delete comment bildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,20 +1,13 @@
 import _ from 'lodash';
 import { isObjectObject } from './tools.js';
 
-/* const getStatus = (obj1, obj2, key) => {
-  if (!_.has(obj1, key)) return 'added';
-  if (!_.has(obj2, key)) return 'removed';
-  if (_.isEqual(obj1[key], obj2[key])) return 'unchanged';
-  return 'changed';
-};
-*/
 const buildDiffTree = (object1, object2) => {
-  const buildDiffObjects = (obj1, obj2) => {
+  const buildDiffNodes = (obj1, obj2) => {
     const allSortsKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 
     return allSortsKeys.flatMap((key) => {
       if (isObjectObject(obj1[key]) && isObjectObject(obj2[key])) {
-        return { name: key, type: 'unchanged', children: buildDiffObjects(obj1[key], obj2[key]) };
+        return { name: key, type: 'unchanged', children: buildDiffNodes(obj1[key], obj2[key]) };
       }
       if (!_.has(obj1, key)) {
         return { name: key, type: 'added', value: obj2[key] };
@@ -31,7 +24,7 @@ const buildDiffTree = (object1, object2) => {
     });
   };
 
-  return { name: '/', type: 'root', children: buildDiffObjects(object1, object2) };
+  return { name: '/', type: 'root', children: buildDiffNodes(object1, object2) };
 };
 
 export default buildDiffTree;
