8beac28c2f397ab40ce9aec141076ba690ad5fbd
android127 <donandrain153@gmail.com>
2023-02-12 18:55:40 +0300

    add spaces

--- src/ differencegenerator.js
+++ src/ differencegenerator.js
@@ -6,10 +6,12 @@ import getDiffInForm from './formatters/index.js';
 const genDiff = (path1, path2, formatName = 'stylish') => {
   const objData1 = parse(readFile(path1), getFileFormat(path1));
   const objData2 = parse(readFile(path2), getFileFormat(path2));
+
   if (!isObjectObject(objData1) || !isObjectObject(objData2)) {
     return 'unknown file format';
   }
   const diffTree = buildDiffTree(objData1, objData2);
+
   return getDiffInForm(diffTree, formatName);
 };
 
