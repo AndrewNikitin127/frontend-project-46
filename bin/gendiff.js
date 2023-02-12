755243b62aa2cb0c7b4d599c07bbd6f13a885bd9
Dushik <donandrain153@gmail.com>
2023-02-12 12:24:56 +0300

    added form selection fund in CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,19 +10,8 @@ program
   .version('1.0.0', '-v, --vers', 'output the current version')
   .arguments('<filepath1> <filepath2>') // обязательно принимает минимум 2 аргумента
   .option('-f, --format <type>', 'output format', 'stylish') // указано значение по дефолту standart
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .action((filepath1, filepath2, option) => {
+    console.log(gendiff(filepath1, filepath2, option.format));
   });
 
 program.parse();
-/*
-
-//тут мы достаем из свойства args объекта program массив аргументов
-const { args } = program;
-//первые два аргумента из массива аргументов присваиваем
-const [file1, file2] = args;
-//получаем объект параметрами, свойства его - указанные нами параметры(format).
-const options = program.opts();
-//получаем указанный формат
-const format = options.format;
-*/
