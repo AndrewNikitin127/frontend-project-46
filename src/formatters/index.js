ff7c1d8a37576bd874cd3b627a9472e12bb8498d
android127 <donandrain153@gmail.com>
2023-03-14 13:19:07 +0500

    add formatters/json.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,11 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const getDiffInForm = (diffTree, formatter) => {
   if (formatter === 'plain') return plain(diffTree);
   if (formatter === 'stylish') return stylish(diffTree);
-  if (formatter === 'json') return JSON.stringify(diffTree);
+  if (formatter === 'json') return json(diffTree);
   return 'unknown formatter';
 };
 
