b83e919d8afa24ebeeee2518577b229e228d6d4d
android127 <donandrain153@gmail.com>
2023-03-22 14:22:18 +0500

    finish change stylish function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,40 +1,41 @@
 import _ from 'lodash';
-import { objectStringify } from '../tools.js';
 
-const indent = (indentCount) => '  '.repeat(indentCount);
-
-const stringify = (nodeValue, indentCount) => {
-  if (!_.isPlainObject(nodeValue)) return String(nodeValue);
-
-  return objectStringify(nodeValue, '  ', indentCount + 2, 2);
+const indent = (depth, spaceCount = 4) => {
+  const numberSpecialSymbols = 2;
+  return ' '.repeat(depth * spaceCount - numberSpecialSymbols);
 };
 
-const mapper = {
-  root: ({ children }, indentCount, iter) => {
-    const textLine = children.flatMap((node) => mapper[node.type](node, indentCount + 1, iter));
+const stringify = (data, depth, mapping) => {
+  if (!_.isPlainObject(data)) return String(data);
+
+  const textLine = Object.entries(data)
+    .map(([name, value]) => mapping.unchanged({ name, value }, depth + 1));
+  return `{\n${textLine.join('\n')}\n${indent(depth)}  }`;
+};
+
+const mapping = {
+  root: ({ children }, depth, iter) => {
+    const textLine = children.flatMap((node) => mapping[node.type](node, depth + 1, iter));
     return `{\n${textLine.join('\n')}\n}`;
   },
-  nested: ({ name, children }, indentCount, iter) => {
-    const textLine = children.flatMap((node) => mapper[node.type](node, indentCount + 2, iter));
-    return `${indent(indentCount)}  ${name}: {\n${textLine.join('\n')}\n${indent(indentCount)}  }`;
+  nested: ({ name, children }, depth, iter) => {
+    const textLine = children.flatMap((node) => mapping[node.type](node, depth + 1, iter));
+    return `${indent(depth)}  ${name}: {\n${textLine.join('\n')}\n${indent(depth)}  }`;
   },
-
-  added: (node, indentCount) => `${indent(indentCount)}+ ${node.name}: ${stringify(node.value, indentCount)}`,
-  removed: (node, indentCount) => `${indent(indentCount)}- ${node.name}: ${stringify(node.value, indentCount)}`,
-  unchanged: (node, indentCount) => `${indent(indentCount)}  ${node.name}: ${stringify(node.value, indentCount)}`,
-
-  changed: (node, indentCount) => {
+  added: (node, depth) => `${indent(depth)}+ ${node.name}: ${stringify(node.value, depth, mapping)}`,
+  removed: (node, depth) => `${indent(depth)}- ${node.name}: ${stringify(node.value, depth, mapping)}`,
+  unchanged: (node, depth) => `${indent(depth)}  ${node.name}: ${stringify(node.value, depth, mapping)}`,
+  changed: (node, depth) => {
     const { name, newValue, oldValue } = node;
 
-    const oldData = `${indent(indentCount)}- ${name}: ${stringify(oldValue, indentCount)}`;
-    const newData = `${indent(indentCount)}+ ${name}: ${stringify(newValue, indentCount)}`;
-
+    const oldData = `${indent(depth)}- ${name}: ${stringify(oldValue, depth, mapping)}`;
+    const newData = `${indent(depth)}+ ${name}: ${stringify(newValue, depth, mapping)}`;
     return [oldData, newData];
   },
 };
 
 const renderTree = (ast) => {
-  const iter = (node, indentCount) => mapper[node.type](node, indentCount, iter);
+  const iter = (node, depth) => mapping[node.type](node, depth, iter);
   return iter(ast, 0);
 };
 
