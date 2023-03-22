7e9772ef095d82beeb9bc02b73184c1f49c05109
android127 <donandrain153@gmail.com>
2023-03-22 16:36:50 +0500

    rename functions stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,17 +16,17 @@ const stringify = (data, depth, mapping) => {
 const getLeafLine = (name, value, depth, mapping, status) => (
   `${indent(depth)}${status} ${name}: ${stringify(value, depth, mapping)}`);
 
-const getParentLine = (children, depth, iter, mapping) => (
+const getChildrenLine = (children, depth, iter, mapping) => (
   children.flatMap((node) => mapping[node.type](node, depth + 1, iter)));
 
 const mapping = {
   root: ({ children }, depth, iter) => {
-    const textLine = getParentLine(children, depth, iter, mapping);
-    return `{\n${textLine.join('\n')}\n}`;
+    const childrenLine = getChildrenLine(children, depth, iter, mapping);
+    return `{\n${childrenLine.join('\n')}\n}`;
   },
   nested: ({ name, children }, depth, iter) => {
-    const textLine = getParentLine(children, depth, iter, mapping);
-    return `${indent(depth)}  ${name}: {\n${textLine.join('\n')}\n${indent(depth)}  }`;
+    const childrenLine = getChildrenLine(children, depth, iter, mapping);
+    return `${indent(depth)}  ${name}: {\n${childrenLine.join('\n')}\n${indent(depth)}  }`;
   },
   added: ({ name, value }, depth) => getLeafLine(name, value, depth, mapping, '+'),
   removed: ({ name, value }, depth) => getLeafLine(name, value, depth, mapping, '-'),
