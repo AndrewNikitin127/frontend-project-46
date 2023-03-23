fe91da66ffe6426a1790afbec5adb34faa8dbe5d
android127 <donandrain153@gmail.com>
2023-03-23 12:51:58 +0500

    add nodeCreateFunc

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,42 +1,43 @@
 import _ from 'lodash';
 
-const getRootNode = (key, type, obj1, obj2, createNodesOfDifference) => (
-  { key, type, children: createNodesOfDifference(obj1, obj2) });
-const getNestedNode = (key, type, obj1, obj2, createNodesOfDifference) => (
-  { key, type, children: createNodesOfDifference(obj1[key], obj2[key]) });
-const getAddedLeafNode = (key, type, obj1, obj2) => (
-  { key, type, value: obj2[key] });
-const getRemovedLeafNode = (key, type, obj1) => (
-  { key, type, value: obj1[key] });
-const getChangedLeafNode = (key, type, obj1, obj2) => (
-  {
+const nodeCreateFunc = {
+  root: (key, type, obj1, obj2, createNodesOfDifference) => (
+    { key, type, children: createNodesOfDifference(obj1, obj2) }),
+  nested: (key, type, obj1, obj2, createNodesOfDifference) => (
+    { key, type, children: createNodesOfDifference(obj1[key], obj2[key]) }),
+  addedLeaf: (key, type, obj1, obj2) => (
+    { key, type, value: obj2[key] }),
+  removedLeaf: (key, type, obj1) => (
+    { key, type, value: obj1[key] }),
+  changedLeaf: (key, type, obj1, obj2) => ({
     key, type, oldValue: obj1[key], newValue: obj2[key],
-  });
+  }),
+};
 
 const nodesActions = [
   {
     type: 'nested',
-    getNode: getNestedNode,
+    getNode: nodeCreateFunc.nested,
     check: (key, obj1, obj2) => _.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key]),
   },
   {
     type: 'added',
-    getNode: getAddedLeafNode,
+    getNode: nodeCreateFunc.addedLeaf,
     check: (key, obj1) => !_.has(obj1, key),
   },
   {
     type: 'removed',
-    getNode: getRemovedLeafNode,
+    getNode: nodeCreateFunc.removedLeaf,
     check: (key, obj1, obj2) => !_.has(obj2, key),
   },
   {
     type: 'unchanged',
-    getNode: getAddedLeafNode,
+    getNode: nodeCreateFunc.addedLeaf,
     check: (key, obj1, obj2) => _.isEqual(obj1[key], obj2[key]),
   },
   {
     type: 'changed',
-    getNode: getChangedLeafNode,
+    getNode: nodeCreateFunc.changedLeaf,
     check: (key, obj1, obj2) => !_.isEqual(obj1[key], obj2[key]),
   },
 ];
@@ -51,7 +52,7 @@ const buildDiffTree = (object1, object2) => {
     });
   };
 
-  return getRootNode('/', 'root', object1, object2, createNodesOfDifference);
+  return nodeCreateFunc.root('/', 'root', object1, object2, createNodesOfDifference);
 };
 
 export default buildDiffTree;
